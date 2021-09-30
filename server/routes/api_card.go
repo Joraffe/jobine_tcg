fe143b1fd486b297ea474cfa6fb7f906343d6fef
Joraffe <jordanwmedina@gmail.com>
2021-09-30 00:39:24 -0700

    [Databse] Add more /api/card routes + db interactions
    - Getting all cards
    - Getting card by a type
    - Getting a card by name
    - Updating a card

--- server/routes/api_card.go
+++ server/routes/api_card.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/Joraffe/jobine_tcg/server/services/db"
 )
@@ -12,10 +13,24 @@ import (
 /*---------------------------------
           Response Data
 ----------------------------------*/
-// CardCreateResponseData is the data we send
+// CardIDResponseData is the data we send
 // back after a successfully creating a new card
-type CardCreateResponseData struct {
+type CardIDResponseData struct {
+	CardID  int64  `json:"cardId"`
+}
+
+
+// CardResponseData is the data we send
+// for fetching an individual card
+type CardResponseData struct {
 	Card  *db.Card  `json:"card"`
+}
+
+
+// CardsResponseData is the data we send back
+// after a successfully getting all card data
+type CardsResponseData struct {
+	Cards  []*db.Card  `json:"cards"`
 }
 
 
@@ -35,12 +50,33 @@ func (r *CardRouter) ServeHTTP(res http.ResponseWriter, req *http.Request) {
 	head, req.URL.Path = ShiftPath(req.URL.Path)
 
 	switch req.Method {
+	// GET Request Handlers
+	case http.MethodGet:
+    switch head {
+		case "all":
+		  r.handleGetAll(res, req)
+		case "":
+			queryParams := req.URL.Query()
+			if param, ok := queryParams["type"]; ok {
+				r.handleGetByType(res, req, strings.Join(param, ""))
+				return
+			}
+			if param, ok := queryParams["name"]; ok {
+				r.handleGetByName(res, req, strings.Join(param, ""))
+				return
+			}
+		default:
+			http.Error(res, fmt.Sprintf("Unsupported GET path %s", head), http.StatusBadRequest)
+			return
+		}
 	// POST Request Handlers
 	case http.MethodPost:
 		// TODO: Add "admin role" type permissions system
 		switch head {
 		case "create":
 			r.handleCreate(res, req)
+		case "update":
+			r.handleUpdate(res, req)
 		default:
 			http.Error(res, fmt.Sprintf("Unsupported POST path %s", head), http.StatusBadRequest)
 			return
@@ -74,7 +110,7 @@ func (r *CardRouter) handleCreate(res http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	card, err := r.Services.Database.CreateCard(cardCreate)
+	cardID, err := r.Services.Database.CreateCard(cardCreate)
 	if err != nil {
 		http.Error(res, fmt.Sprintf("Error creating new card in database: %s", err.Error()), http.StatusInternalServerError)
 		return
@@ -83,8 +119,97 @@ func (r *CardRouter) handleCreate(res http.ResponseWriter, req *http.Request) {
 	response := &Response{
 		Success: true,
 		Error:   nil,
-		Data:    CardCreateResponseData{
-			Card:  card,
+		Data:    CardIDResponseData{
+			CardID:  cardID,
+		},
+	}
+
+	res.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(res).Encode(response)
+}
+
+
+func (r *CardRouter) handleUpdate(res http.ResponseWriter, req *http.Request) {
+	decoder := json.NewDecoder(req.Body)
+	cardUpdate := new(db.CardUpdate)
+
+	err := decoder.Decode(cardUpdate)
+	if err != nil {
+		http.Error(res, fmt.Sprintf("Invalid JSON request: %s", err.Error()), http.StatusBadRequest)
+		return
+	}
+
+	cardID, err := r.Services.Database.UpdateCard(cardUpdate)
+	if err != nil {
+		http.Error(res, fmt.Sprintf("Error updating card in database: %s", err.Error()), http.StatusInternalServerError)
+		return
+	}
+
+	response := &Response{
+		Success:   true,
+		Error:     nil,
+		Data:      CardIDResponseData{
+			CardID:  cardID,
+		},
+	}
+
+	res.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(res).Encode(response)
+}
+
+
+func (r *CardRouter) handleGetAll(res http.ResponseWriter, req *http.Request) {
+	cards, err := r.Services.Database.GetAllCards()
+	if err != nil {
+		http.Error(res, fmt.Sprintf("Error getting all cards from DB: %s", err.Error()), http.StatusInternalServerError)
+		return
+	}
+
+	response := &Response{
+		Success:  true,
+		Error:    nil,
+		Data:     CardsResponseData{
+			Cards:  cards,
+		},
+	}
+
+	res.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(res).Encode(response)
+}
+
+
+func (r *CardRouter) handleGetByType(res http.ResponseWriter, req *http.Request, typeParam string) {
+	cards, err := r.Services.Database.GetCardsByCardType(typeParam)
+	if err != nil {
+		http.Error(res, fmt.Sprintf("Error getting all cards for given type from DB: %s", err.Error()), http.StatusInternalServerError)
+		return
+	}
+
+	response := &Response{
+		Success:  true,
+		Error:    nil,
+		Data:     CardsResponseData{
+			Cards:  cards,
+		},
+	}
+
+	res.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(res).Encode(response)
+}
+
+
+func (r *CardRouter) handleGetByName(res http.ResponseWriter, req *http.Request, nameParam string) {
+	cards, err := r.Services.Database.GetCardsByCardType(nameParam)
+	if err != nil {
+		http.Error(res, fmt.Sprintf("Error getting card with name %s from DB: %s", nameParam, err.Error()), http.StatusInternalServerError)
+		return
+	}
+
+	response := &Response{
+		Success:  true,
+		Error:    nil,
+		Data:     CardsResponseData{
+			Cards:   cards,
 		},
 	}
 
--- server/services/db/card.go
+++ server/services/db/card.go
@@ -7,7 +7,12 @@ package db
 // CardManager describes all of the methods used
 // to interact with the card table in our database
 type CardManager interface {
-	CreateCard(cardCreate *CardCreate) (*Card, error)
+	GetAllCards() ([]*Card, error)
+	GetCardsByCardType(cardType string) ([]*Card, error)
+	GetCardByCardName(cardName string) (*Card, error)
+
+	CreateCard(cardCreate *CardCreate) (int64, error)
+	UpdateCard(cardUpdate *CardUpdate) (int64, error)
 }
 
 
@@ -42,18 +47,28 @@ type CardCreate struct {
 	CardEndurance     NullInt64JSON   `json:"cardEndurance,omitempty"`
 }
 
+// CardUpdate describes the data needed 
+// to update a given card in our db
+type CardUpdate struct {
+	CardID            int64           `json:"cardId,omitempty"`
+	CardName          NullStringJSON  `json:"cardName,omitempty"`
+	CardType          NullStringJSON  `json:"cardType,omitempty"`
+	CardArtist        NullStringJSON  `json:"cardArtist,omitempty"`
+	CardEffect        NullStringJSON  `json:"cardEffect,omitempty"`
+	CardPower         NullInt64JSON   `json:"cardPower,omitempty"`
+	CardIntelligence  NullInt64JSON   `json:"cardIntelligence,omitempty"`
+	CardEndurance     NullInt64JSON   `json:"cardEndurance,omitempty"`
+}
+
 
 /*---------------------------------
        Method Implementations
 ----------------------------------*/
-// CreateCard adds a new entry to the card table in our database
-func (db *DB) CreateCard(cardCreate *CardCreate) (*Card, error) {
+
+// GetAllCards fetches all cards in our db
+func (db *DB) GetAllCards() ([]*Card, error) {
 	sqlStatement := `
-	  INSERT INTO cards
-		  (card_name, card_type, card_artist, card_effect, card_power, card_intelligence, card_endurance)
-		VALUES
-		  ($1, $2, $3, $4, $5, $6, $7)
-		RETURNING
+	  SELECT
 		  card_id,
 			card_name,
 			card_type,
@@ -62,18 +77,112 @@ func (db *DB) CreateCard(cardCreate *CardCreate) (*Card, error) {
 			card_power,
 			card_intelligence,
 			card_endurance
+		FROM
+		  cards
 	`
-	row := db.QueryRow(
-		sqlStatement,
-		cardCreate.CardName,
-		cardCreate.CardType,
-		cardCreate.CardArtist,
-		cardCreate.CardEffect,
-		cardCreate.CardPower,
-		cardCreate.CardIntelligence,
-		cardCreate.CardEndurance,
-	)
+	rows, err := db.Query(sqlStatement)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
 
+	cards := make([]*Card, 0)
+	for rows.Next() {
+		card := new(Card)
+		err := rows.Scan(
+			&card.CardID,
+			&card.CardName,
+			&card.CardType,
+			&card.CardArtist,
+			&card.CardEffect,
+			&card.CardPower,
+			&card.CardIntelligence,
+			&card.CardEndurance,
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		cards = append(cards, card)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		return nil, err
+	}
+
+	return cards, nil
+}
+
+
+// GetCardsByCardType fetches all cards with a specific type
+func (db *DB) GetCardsByCardType(cardType string) ([]*Card, error) {
+	sqlStatement := `
+	  SELECT
+		  card_id,
+			card_name,
+			card_type,
+			card_artist,
+			card_effect,
+			card_power,
+			card_intelligence,
+			card_endurance
+		FROM
+		  cards
+		WHERE
+		  card_type = $1
+	`
+	rows, err := db.Query(sqlStatement, cardType)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	cards := make([]*Card, 0)
+	for rows.Next() {
+		card := new(Card)
+		err := rows.Scan(
+			&card.CardID,
+			&card.CardName,
+			&card.CardType,
+			&card.CardArtist,
+			&card.CardEffect,
+			&card.CardPower,
+			&card.CardIntelligence,
+			&card.CardEndurance,
+		)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	err = rows.Err()
+	if err != nil {
+		return nil, err
+	}
+
+	return cards, nil
+}
+
+
+// GetCardByCardName fetches a specific card by its name from the db
+func (db *DB) GetCardByCardName(cardName string) (*Card, error) {
+	sqlStatement := `
+	  SELECT
+			card_id,
+			card_name,
+			card_type,
+			card_artist,
+			card_effect,
+			card_power,
+			card_intelligence,
+			card_endurance
+		FROM
+		  cards
+		WHERE
+		  card_name = $1
+	`
+	row := db.QueryRow(sqlStatement, cardName)
 	card := new(Card)
 	err := row.Scan(
 		&card.CardID,
@@ -90,4 +199,75 @@ func (db *DB) CreateCard(cardCreate *CardCreate) (*Card, error) {
 	}
 
 	return card, nil
+}
+
+
+// CreateCard adds a new entry to the card table in our database
+func (db *DB) CreateCard(cardCreate *CardCreate) (int64, error) {
+	var cardID int64
+	sqlStatement := `
+	  INSERT INTO cards
+		  (card_name, card_type, card_artist, card_effect, card_power, card_intelligence, card_endurance)
+		VALUES
+		  ($1, $2, $3, $4, $5, $6, $7)
+		RETURNING
+		  card_id
+	`
+	row := db.QueryRow(
+		sqlStatement,
+		cardCreate.CardName,
+		cardCreate.CardType,
+		cardCreate.CardArtist,
+		cardCreate.CardEffect,
+		cardCreate.CardPower,
+		cardCreate.CardIntelligence,
+		cardCreate.CardEndurance,
+	)
+
+	err := row.Scan(&cardID)
+	if err != nil {
+		return 0, err
+	}
+
+	return cardID, nil
+}
+
+
+// UpdateCard updates an existing entry in the cards table
+func (db *DB) UpdateCard(cardUpdate *CardUpdate) (int64, error) {
+	var cardID int64
+	sqlStatement := `
+	  UPDATE
+		  cards
+		SET
+		  card_name = $1,
+			card_type = $2,
+			card_artist = $3,
+			card_effect = $4,
+			card_power = $5,
+			card_intelligence = $6,
+			card_endurance = $7
+		WHERE
+		  card_id = $8
+		RETURNING
+		  card_id
+	`
+
+	row := db.QueryRow(
+		sqlStatement,
+		cardUpdate.CardName,
+		cardUpdate.CardType,
+		cardUpdate.CardArtist,
+		cardUpdate.CardEffect,
+		cardUpdate.CardPower,
+		cardUpdate.CardIntelligence,
+		cardUpdate.CardEndurance,
+		cardUpdate.CardID,
+	)
+	err := row.Scan(&cardID)
+	if err != nil {
+		return 0, err
+	}
+
+	return cardID, nil
 }
